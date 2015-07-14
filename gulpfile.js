fc7dcd8bd23fe10caaa4fbeccd786210499b531e
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-13 19:38:38 -0700

    gulpified livereload n watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-re('gulp');
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
 });
-e
+
+gulp.task('reload', function() {
+  gulp.src('*.html')
+    .pipe(connect.reload());
+
+  console.log('detected change');
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./*.html', ['reload']);
+})
+
+gulp.task('default', ['webserver', 'watch']);
