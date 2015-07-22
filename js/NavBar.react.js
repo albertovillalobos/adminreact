89394e6313abeb76f002aada314010a61430fe15
Alberto Villalobos <alberto@albertovillalobos.me>
2015-07-22 16:55:54 -0700

    starting to refactor app

---
+++ js/NavBar.react.js
@@ -0,0 +1,37 @@
+var React = require('react');
+
+var NavBar = React.createClass({
+
+  render: function() {
+
+    return(
+
+      <div className="theNavbar header">
+        <div className="navbar navbar-inverse" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#js-navbar-collapse">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+
+              <a className="navbar-brand" href="/#/">AdminReact</a>
+            </div>
+
+            <div className="collapse navbar-collapse" id="js-navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="/#/analytics"><span className="glyphicon glyphicon-stats" aria-hidden="true"></span> Analytics</a></li>
+                <li className=""><a href="/#/coupons"><span className="glyphicon glyphicon-qrcode" aria-hidden="true"></span> Coupons</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  },
+});
+
+module.exports = NavBar;
--- js/app.js
+++ js/app.js
@@ -5,20 +5,12 @@ var Router = require('react-router');
 var Route = Router.Route;
 var RouteHandler = Router.RouteHandler;
 
-
 Parse.initialize("fUnC8PIBgPR26VUGhbsZFH4tStFUFyOZJ6baLo8O", "CkPiEsxSHfqtriaJ266t2yknRXArxBy1lVs5WQvI");
 
-var App = React.createClass({
+var NavBar = require('./NavBar.react.js');
+var NotFound = require('./NotFound.react.js');
 
-  render() {
-    return(
-      <div>
-        <NavBar/>
-        <AnalyticsContainer/>
-      </div>
-    )
-  }
-});
+
 
 var RoutedApp = React.createClass({
   render(){
@@ -31,41 +23,6 @@ var RoutedApp = React.createClass({
   }
 })
 
-
-// Nav bar
-var NavBar = React.createClass({
-
-  render: function() {
-
-    return(
-
-      <div className="theNavbar header">
-        <div className="navbar navbar-inverse" role="navigation">
-          <div className="container">
-            <div className="navbar-header">
-
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#js-navbar-collapse">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-
-              <a className="navbar-brand" href="/#/">AdminReact</a>
-            </div>
-
-            <div className="collapse navbar-collapse" id="js-navbar-collapse">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="/#/analytics"><span className="glyphicon glyphicon-stats" aria-hidden="true"></span> Analytics</a></li>
-                <li className=""><a href="/#/coupons"><span className="glyphicon glyphicon-qrcode" aria-hidden="true"></span> Coupons</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  },
-});
 
 
 // Analytics page and its components
@@ -163,24 +120,6 @@ var MonthlyProfits = React.createClass({
 });
 
 
-
-
-
-// var AnalyticsContainer = React.createClass({
-//
-//   render() {
-//     return(
-//       <div className="container">
-//         <div className="row">
-//           <CouponCounter/>
-//           <DailyProfits/>
-//           <WeeklyProfits/>
-//           <MonthlyProfits/>
-//         </div>
-//       </div>
-//     )
-//   }
-// });
 
 // Coupons page and its components
 var CouponsContainer = React.createClass({
@@ -321,18 +260,8 @@ var Index = React.createClass({
   }
 });
 
-var Coupons = React.createClass({
-  render() {
-    return (<h1>Welcome to the Coupon panel</h1>);
-  }
-});
 
 
-var NotFound = React.createClass({
-  render() {
-    return (<h2>Not found</h2>);
-  }
-});
 
 
 var routes = (
